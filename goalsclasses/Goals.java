4c10e88a5e7bd72beff8b53973bb40f746fee94b
David Sage <davidsage@vt.edu>
2026-09-15 15:57:33 -0400

    commented goals class

--- goalsclasses/Goals.java
+++ goalsclasses/Goals.java
@@ -1,16 +1,23 @@
 package goalsclasses;
 /*
-docstring class
+    The Goals class acts as the parent class of all goals and investments,
+    it holds information about the cost of the goal and if the goal is met.
+    Child classes follow specific goals and contain more information while
+    the parent holds most the information for the user class to know and get.
 
 */
 public class Goals {
     // Fields
     private boolean transfersToNextMonth;
-    private boolean goalMet;
+    private boolean goalMet = false;
     private String name;
     private double cost;
 
-    // Constructor
+    // Constructors
+    /*
+        Constructs goals without specifying if it transfers month to month,
+        assumes not if not specified and is overloaded.
+    */
     public Goals(String name, double cost) {
         this(name,cost,false);
     }
@@ -22,22 +29,45 @@ public class Goals {
     }
 
     // Methods
+    /*
+        @return name 
+        - name of the goal
+    */
     public String getName() {
         return name;
     }
-
+    /*
+        @return transfersToNextMonth
+        - boolean on if the goal is carried to the following month
+    */
     public boolean getTransfersToNextMonth() {
         return transfersToNextMonth;
     }
-    
+    /*
+        @return goalMet
+        - boolean on if the goal has been completed
+    */
     public boolean goalMet() {
         return goalMet;
     }
-
+    /*
+        Called by children to declare goal has been met
+    */
+    protected void metGoal() {
+        goalMet = true;
+    }
+    /*
+        @return cost 
+        - gives the cost of the goal
+    */
     public double getCost() {
         return cost;
     }
-
+    /*
+        sets cost of the goal
+        @param cost
+        - cost of goal
+    */
     public void setCost(double cost){
         this.cost = cost;
     }
