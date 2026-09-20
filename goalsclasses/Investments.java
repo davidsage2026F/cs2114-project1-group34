33ccb98c3bb9c5b76ee5a506f467b3873abeb808
David <115376504+BossySauce@users.noreply.github.com>
2026-09-20 15:59:34 -0400

    almost finished goals package

--- goalsclasses/Investments.java
+++ goalsclasses/Investments.java
@@ -1,39 +1,37 @@
 package goalsclasses;
+/*
+    OneTimePurchase class specifies that the goal is a purchase made once
+    with an overpay variable in case the user pays more than the goal
+    costs. Updates automatically if the goal is met and allows for the
+    user to add funding over time.
 
+*/
 public class Investments extends Goals{
 
     // Fields
     private double amountInvested;
     private double yearlyROI;
-    private int timeFrame;
 
     // Constructor
-    public Investment(double amountInvested, double yearlyROI, int timeFrame){
+    public Investments(String name, double amountInvested, double yearlyROI){
+        super(name, -1.0, false);
         this.amountInvested = amountInvested;
         this.yearlyROI = yearlyROI;
-        this.timeFrame = timeFrame;
+    }
+
+    public Investments(String name, double amountInvested) {
+        super(name, -1, false);
+        this.amountInvested = amountInvested;
+        this.yearlyROI = 0.08;
     }
 
     // Getter and setter methods
-    public void setAmountInvested(amountInvested){
-        this.amountInvested = amountInvested;
-    }
     public double getAmountInvested(){
         return amountInvested;
     }
 
-    public void setYearlyReturnOnInvestment(yearlyROI){
-        this.yearlyROI = yearlyROI;
-    }
-    public int getYearlyReturnOnInvestment(){
+    public double getYearlyROI(){
         return yearlyROI;
-    }
-
-    public void setTimeFrame(timeFrame){
-        this.timeFrame = timeFrame
-    }
-    public int getTimeFrame(return timeFrame){
-        return timeFrame;
     }
 
     // Methods
@@ -41,6 +39,29 @@ public class Investments extends Goals{
         amountInvested = amountInvested + investAmount;
     }
 
+    public double[] calculateInvestment(int monthsIntoFuture) {
+        double[] amounts = new double[10];
+        for (int i = 1; i <= 10; i++) {
+            amounts[i] = monthAmount((monthsIntoFuture / 10) * i);
+        }
+        return amounts;
+    }
 
+    public void updateInvestment() {
+        addToInvestment(amountInvested * (yearlyROI / 12));
+    }
+
+    private double monthAmount(int month) {
+        return amountInvested * (yearlyROI / 12) * month;
+    }
+
+    @Override
+    public double getCost() {
+        return getAmountInvested();
+    }
     
+    @Override
+    public String toString() {
+        return "Investment " + this.getName() + " with a yearly ROI of " + yearlyROI * 100 + "% and an amount invested of $" + amountInvested + ".";
+    }
 }
